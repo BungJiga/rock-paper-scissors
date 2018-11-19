057dbf4026a12d53a2fa6b5db9a2bde4b0c90bc2
Michael Guerrero <michaelguerrero@uagateway.org>
2018-11-19 22:34:35 +0000

    Jodie is the best

--- README.md
+++ README.md
@@ -40,14 +40,14 @@ In this unit, coders will create a Rock, Paper, Scissors game. The Rock, Paper, 
 ## Day 3
 
 ## JavaScript
-- [ ] Write a conditional statement which, given the number range of randomNumber, assigns ‘rock’, ‘paper’ or ‘scissors’ to the computerChoice variable 
-- [ ] Display the computerChoice to the screen
-- [ ] Write a conditional statement that compares the userChoice to the computerChoice to determine the winner of the game
-- [ ] Display the winner to the screen in the result div
-- [ ] Clear the input value once a result is displayed
+- [x] Write a conditional statement which, given the number range of randomNumber, assigns ‘rock’, ‘paper’ or ‘scissors’ to the computerChoice variable 
+- [x] Display the computerChoice to the screen
+- [x] Write a conditional statement that compares the userChoice to the computerChoice to determine the winner of the game
+- [x] Display the winner to the screen in the result div
+- [x] Clear the input value once a result is displayed
 
 #### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 ## Projects Extensions:
 - [ ] Style the page to fit your personality
--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
-var userChoice;
-var computerChoice;
-var winner;
-var randomNumber = 0;
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = Math.floor((Math.random() * 3) + 1);
 
 $(document).ready(function(){
     
@@ -21,5 +21,85 @@ $("#shoot").click(function(){
 userChoice = $("#input").val();
 $("#userChoice").text(userChoice);
 
-    }); 
+   
+    
+    if(randomNumber == 1 ){
+        
+        computerChoice = "rock";
+        $("#computerChoice").text(computerChoice);
+        
+        
+    }
+    if(randomNumber == 2 ){
+        
+        computerChoice = "paper";
+          $("#computerChoice").text(computerChoice);
+        
+    }
+    if(randomNumber == 3 ){
+        
+        computerChoice = "scissors";
+          $("#computerChoice").text(computerChoice);
+        
+    }
+    
+     if (computerChoice == "paper" && userChoice == "rock"){
+        
+        
+        $("#result").text("You  Lose!");
+        
+    }
+    
+    if (computerChoice == "rock" && userChoice == "paper"){
+        
+        
+        $("#result").text("You Win!");
+        
+    }
+       if (computerChoice == "rock" && userChoice == "scissors"){
+        
+        
+        $("#result").text("You Lose!");
+        
+    }
+    
+    if (computerChoice == "scissors" && userChoice == "rock"){
+        
+        
+        $("#result").text("You Win!");
+        
+    }
+       if (computerChoice == "scissors" && userChoice == "paper"){
+        
+        
+        $("#result").text("You Lose!");
+        
+    }
+    
+    if (computerChoice == "paper" && userChoice == "scissors"){
+        
+        
+        $("#result").text("You Win!");
+        
+    }
+     if (computerChoice == "paper" && userChoice == "paper"){
+        
+        
+        $("#result").text("Tie!");
+        
+    }
+    
+    if (computerChoice == "scissors" && userChoice == "scissors"){
+        
+        
+        $("#result").text("Tie!");
+        
+    }
+     if (computerChoice == "rock" && userChoice == "rock"){
+        
+        
+        $("#result").text("Tie!");
+        
+    }
+}); 
 });
